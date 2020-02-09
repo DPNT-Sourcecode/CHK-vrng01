63cc5f1acb623bc2aa531c4c7315da2c01316a96
sbx_user1051 <sbx_user1051@169.254.2.25>
2020-02-09 13:22:28 +0000

    Sun Feb 09 13:22:28 UTC 2020

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -52,45 +52,13 @@ namespace BeFaster.App.Solutions.CHK
                     case 'E':
                         break;
                 }
-                if (key.Equals('A'))
-                {
-                    //switch (val)
-                    //{
-                    //    case 
-                    //}
-                }
-                //switch (count)
-                //{
-                //    case 3 when c.Equals('A'):
-                //        total += 130;
-                //        SKUs[c] = 0;
-                //        break;
-                //    case 2 when c.Equals('B'):
-                //        total += 45;
-                //        SKUs[c] = 0;
-                //        break;
-                //}
-                //switch (sku.Key)
-                //{
-                //    case 'A':
-                //        total += count * 50;
-                //        break;
-                //    case 'B':
-                //        total += count * 30;
-                //        break;
-                //    case 'C':
-                //        total += count * 20;
-                //        break;
-                //    case 'D':
-                //        total += count * 15;
-                //        break;
-                //}
             }
 
             return total;
         }
     }
 }
+
 
 
 
