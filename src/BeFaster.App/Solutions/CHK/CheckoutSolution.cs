ef892292717dbb0f1f47ead001cee498fb213bd2
sbx_user1051 <sbx_user1051@169.254.2.25>
2020-02-09 12:33:26 +0000

    Sun Feb 09 12:33:26 UTC 2020

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,12 +1,15 @@
-﻿using BeFaster.Runner.Exceptions;
+﻿using System.Collections.Generic;
+using BeFaster.Runner.Exceptions;
 
 namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
+        private Dictionary<char, int> SKUS = new Dictionary<char, int>();
         public static int ComputePrice(string skus)
         {
             throw new SolutionNotImplementedException();
         }
     }
 }
+
